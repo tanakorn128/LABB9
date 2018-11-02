76faf85a2205cd3f81cb963631e4018ae7af3b83
tanakorn128 <60030041@kmitl.ac.th>
2018-11-03 01:52:42 +0700

    ok

---
+++ 3.5.11 การวาดสี่เหลี่ยมครั้งละรูป/3.5.11 การวาดสี่เหลี่ยมครั้งละรูป/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._5._11_การวาดสี่เหลี่ยมครั้งละรูป
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Blue, 3);
+            g.DrawRectangle(myPen, 10, 120, 100, 100);
+
+            Rectangle rect = new Rectangle(10, 10, 100, 100);
+            g.DrawRectangle(myPen, rect);
+            g.Dispose();
+        }
+    }
+}
