756e89780b5dadfa0d4166cb94b9966560eff053
tanakorn128 <60030041@kmitl.ac.th>
2018-11-03 02:05:51 +0700

    ok

---
+++ 3.5.2การวาดสี่เหลี่ยมพร้อมกันครั้งละหลายๆ รูป/3.5.2การวาดสี่เหลี่ยมพร้อมกันครั้งละหลายๆ รูป/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._5._2การวาดสี่เหลี่ยมพร้อมกันครั้งละหลายๆ_รูป
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.Blue, 3);
+            Rectangle[] rects =
+            {
+                new Rectangle(20,20,120,29),
+                new Rectangle(20,50,120,30),
+                new Rectangle(20,90,120,40),
+                new Rectangle(20,140,120,60)
+            };
+            g.DrawRectangles(myPen, rects);
+            g.Dispose();
+        }
+    }
+}
---
+++ 3.5.2การวาดสี่เหลี่ยมพร้อมกันครั้งละหลายๆ รูป/3.5.2การวาดสี่เหลี่ยมพร้อมกันครั้งละหลายๆ รูป/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("3.5.2การวาดสี่เหลี่ยมพร้อมกันครั้งละหลายๆ รูป")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("3.5.2การวาดสี่เหลี่ยมพร้อมกันครั้งละหลายๆ รูป")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("24d3cac2-6afe-43c4-9007-0a9f5f58b4f3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
