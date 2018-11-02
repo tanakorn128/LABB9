1053ccafdf0cbd5f1282cec105ce0b8080b0f737
tanakorn128 <60030041@kmitl.ac.th>
2018-11-03 02:21:53 +0700

    ok

---
+++ 3.5.3การวาดวงกลมและวงรี/3.5.3การวาดวงกลมและวงรี/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._5._3การวาดวงกลมและวงรี
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            // draw circle
+            Rectangle rect = new Rectangle(10, 10, 100, 100);
+            g.DrawEllipse(Pens.Purple, rect);
+
+            //Fill circle
+            Rectangle rect1 = new Rectangle(10, 120, 100, 100);
+            g.FillEllipse(Brushes.DeepPink, rect1);
+
+            //Draw ellipse
+            Rectangle rect2 = new Rectangle(150, 10, 120, 100);
+            g.DrawEllipse(Pens.DarkSlateBlue, rect2);
+
+            //fill ellipse
+            Rectangle rect3 = new Rectangle(150, 120, 120, 100);
+            g.FillEllipse(Brushes.Firebrick, rect3);
+
+            g.Dispose();
+        }
+    }
+}
