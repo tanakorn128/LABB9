b53c2577ee4cdf72b1f5f6e889393a3bba23a97c
tanakorn128 <60030041@kmitl.ac.th>
2018-11-03 01:02:36 +0700

    ok

---
+++ การวาดเส้นตรง3.3/การวาดเส้นตรง3.3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace การวาดเส้นตรง3._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            Point point1 = new Point(100, 100);
+            Point point2 = new Point(200, 100);
+            e.Graphics.DrawLine(blackPen, point1, point2);
+            blackPen.Dispose();
+        }
+    }
+}
