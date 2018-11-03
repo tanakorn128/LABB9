858cd6bf8e33b49d672bd7095bc40a07c030c3ae
tanakorn128 <60030041@kmitl.ac.th>
2018-11-03 19:02:19 +0700

    update3_1_2561

---
+++ 3.3.5การสร้าง graphics path จากรูปต่างๆ/3.3.5การสร้าง graphics path จากรูปต่างๆ/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace _3._3._5การสร้าง_graphics_path_จากรูปต่างๆ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            GraphicsPath gpath = new GraphicsPath();
+            gpath.AddEllipse(46, 4, 28, 28);
+            gpath.AddLine(36,32,84,32);
+            gpath.AddLine(100,80,88,84);
+            gpath.AddLine(76,50,74,84);
+            gpath.AddLine(90,150,74,150);
+            gpath.AddLine(60,100,46,150);
+            gpath.AddLine(32,150,46,84);
+            gpath.AddLine(44,50,32,84);
+            gpath.AddLine(20,80,36,32);
+            g.FillPath(Brushes.Blue, gpath);
+            g.Dispose();
+        }
+    }
+}
---
+++ 3.3.5การสร้าง graphics path จากรูปต่างๆ/3.3.5การสร้าง graphics path จากรูปต่างๆ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._3._5การสร้าง_graphics_path_จากรูปต่างๆ
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
