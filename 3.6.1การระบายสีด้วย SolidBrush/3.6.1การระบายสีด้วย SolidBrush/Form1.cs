c664132fa96a0f515b6b3ef9d75ce07e81985c0d
tanakorn128 <60030041@kmitl.ac.th>
2018-11-03 20:07:00 +0700

    ok

---
+++ 3.6.1การระบายสีด้วย SolidBrush/3.6.1การระบายสีด้วย SolidBrush/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace _3._6._1การระบายสีด้วย_SolidBrush
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Point[] pt = { new Point(10,22),
+            new Point(188,246),
+            new Point(250,192),
+            new Point(220,48)};
+
+            g.FillClosedCurve(Brushes.Blue, pt);
+            g.DrawClosedCurve(Pens.Red, pt);
+            g.Dispose();
+        }
+    }
+}
