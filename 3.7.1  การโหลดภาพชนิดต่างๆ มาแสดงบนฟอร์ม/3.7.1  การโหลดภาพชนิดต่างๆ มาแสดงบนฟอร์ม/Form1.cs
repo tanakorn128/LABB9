082e68eb5b8cb4b1e0c44bf17c33f34669b6b22f
tanakorn128 <60030041@kmitl.ac.th>
2018-11-06 16:46:35 +0700

    ok

---
+++ 3.7.1  การโหลดภาพชนิดต่างๆ มาแสดงบนฟอร์ม/3.7.1  การโหลดภาพชนิดต่างๆ มาแสดงบนฟอร์ม/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace _3._7._1__การโหลดภาพชนิดต่างๆ_มาแสดงบนฟอร์ม
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Bitmap Bmp = new Bitmap("D:\\Capture.PNG");
+            Rectangle destrect = new Rectangle(10, 10, Bmp.Width / 2, Bmp.Height / 2);
+            Rectangle srcrect = new Rectangle(0,0,Bmp.Width,Bmp.Height);
+            this.SetClientSizeCore(destrect.Width + 20, destrect.Height + 20);
+            g.DrawImage(Bmp, destrect, srcrect, GraphicsUnit.Pixel);
+            g.Dispose();
+        }
+    }
+}
---
+++ 3.7.1  การโหลดภาพชนิดต่างๆ มาแสดงบนฟอร์ม/3.7.1  การโหลดภาพชนิดต่างๆ มาแสดงบนฟอร์ม/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3._7._1__การโหลดภาพชนิดต่างๆ_มาแสดงบนฟอร์ม
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
